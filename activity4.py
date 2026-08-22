bffd1eb9c1bdc24200b4ac18a87742dbd57d284a
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-08-22 18:31:36 +0800

    fourth

---
+++ activity4.py
@@ -0,0 +1,10 @@
+#\n=new line
+#\t=tab
+#\r=carriage return
+#\\=insert slash
+#\"=insert double quote
+#\b=back space
+
+motto="The Quick \rBrown Fox Jumps\t\t\t over the Lazy Dog. A \nnegative mind would never\b\b\b\b\b give you a \\positive\" life"
+
+print(motto)
