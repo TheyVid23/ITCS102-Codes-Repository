fc5d543438d4182e6d4b5a069ee51750af31e38f
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-08-22 18:24:08 +0800

    third

---
+++ activity3.py
@@ -0,0 +1,24 @@
+#Strings
+name="David Anthony P. Balubal"
+address="Purok 5, Sto Cristo, Sariaya Quezon"
+message="Hi Baby"
+
+#Boolean
+isHandsome=True
+isSmart=True
+isMatchaLover=True
+
+#Integers
+HouseNumber=17
+HorseShoe=36
+CoolingFan=1
+
+#Float
+ShoeSize=7.6
+Grades=99.9
+Weight=88.9
+
+print("Hi, my name is", name)
+print("Am I Handsome?", isHandsome)
+print("This is my House Number,", HouseNumber)
+print("My shoe size is", ShoeSize)
