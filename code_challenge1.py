d6b225bd89f9fc8091edbec1d558001cf783bdb4
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-08-22 18:55:30 +0800

    fifth_code_challenge_1

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+print("\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\tHi! My name is\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\\\"DAVID ANTHONY P BALUBAL\"\\\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
+
+   
