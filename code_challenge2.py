8068e49aed35544e5d8366d111423e9ecfcd9e3c
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-08-25 16:06:19 +0800

    wowawie

---
+++ code_challenge2.py
@@ -0,0 +1,41 @@
+money = 2304
+
+danilo = money // 1000
+remaining = money % 1000
+
+angelica = remaining // 500
+remaining = remaining % 500
+
+dianne = remaining // 200
+remaining = remaining % 200
+
+annalyn = remaining // 100
+remaining = remaining % 100
+
+david = remaining // 50
+remaining = remaining % 50
+
+justine = remaining // 20
+remaining = remaining % 20
+
+nita = remaining // 10
+remaining = remaining % 10
+
+mae = remaining // 5
+remaining = remaining % 5
+
+jhoree = remaining // 1
+
+print("The money deposited is", money)
+print("1000 =", danilo)
+print("500 =", angelica)
+print("200 =", dianne)
+print("100 =", annalyn)
+print("50 =", david)
+print("20 =", justine)
+print("10 =", nita)
+print("5 =", mae)
+print("1 =", jhoree)
+
+
+
