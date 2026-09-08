be270d418756be49e3b1196fd6fbcfa5edf2e365
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-09-08 12:09:55 +0800

    horray

---
+++ activity10.py
@@ -0,0 +1,7 @@
+a = 4
+b = 30 
+c = 23
+
+print(a > b or c < a)
+print(a > b or c < a and c == b)
+print(c < a and c == b)
---
+++ activity9.py
@@ -0,0 +1,20 @@
+#relational operators
+x = 67
+y = 76
+text1 = 'Mamamo'
+text2 = 'mamamo'
+#1
+print(x > y)
+#2
+print(x < y)
+#3
+print(x >= y)
+#4
+print(x <= y)
+#5
+print(x == y)
+print(text1 == text2)
+#6
+print(x != y)
+print(text1 != text2)
+
