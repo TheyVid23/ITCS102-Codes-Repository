35a734734ad900ca49a94a35c66ec3d9ac0764b4
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-09-08 12:14:18 +0800

    Add basic arithmetic operations in activity5.py

---
+++ activity5.py
@@ -0,0 +1,18 @@
+a = 23
+b = 4
+sum = a + b 
+diff = a - b
+product = a*b
+quotient = a/b
+exponential = a**b
+floordivision = a//b
+modulus = a%b
+
+
+print("The sum of",a,"plus",b,"is",sum)
+print("The difference of",a,"minus",b,"is",diff)
+print("The product of",a,"times",b,"is",product)
+print("The quotient of",a,"divide",b,"is",quotient)
+print("The result of",a,"raise to",b,"is",exponential)
+print("The result of",a,"floordivision",b,"is",floordivision)
+print("The result of",a,"modulus",b,"is",modulus)
