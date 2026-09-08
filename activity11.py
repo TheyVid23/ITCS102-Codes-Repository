af8bb54ae09b1d36477afbd07a0865e2fe8c8213
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-09-08 12:32:44 +0800

    kissmokolozabia

---
+++ activity11.py
@@ -0,0 +1,10 @@
+username = 'userako'
+password = 'davidmapagmahalx3d'
+
+u = input('Input username --> ')
+p = input('Input password --> ')
+
+if username == u and p == password:
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
