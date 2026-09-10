6ef3c099ab667e314e9007e57b8550aacc1ff697
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-09-10 12:56:04 +0800

    ewan

---
+++ code_challenge3.py
@@ -0,0 +1,54 @@
+SenderName = input('What is your name? --> ')
+
+TypeOfItem = input('What is your order? --> ')
+
+is_Fragile = bool(input('Is it fragile? --> '))
+
+#if is_Fragile == True:
+	print('It is')
+#else:
+	print('It is not')
+
+Weight = float(input('How heavy is it? --> ')) 
+
+Distance = float(input('How far is it from the designated destination? --> '))
+
+is_Express = bool(input('Is it in a hurry? --> '))
+
+#if is_Express == True:
+	print('It is')
+#else:
+	print('It is not')
+
+is_International = bool(input('Is it in from another country? --> '))
+
+#if is_International == True:
+	print('It is')
+#else:
+	print('It is not')
+
+base_cost = (weight * 2.5) + (distance * 0.15)
+
+InternationalExpress == Total1
+	Total1 = (base_cost * 1.4) + 50
+
+HeavyInternational == Total2 
+	Total2 = (base_cost * 1.2) + 25
+
+Oversized == Total3
+	Total3 = base_cost + 30
+
+StandardRate == Total4
+	Total4 = base_cost
+
+Price = Total
+ALL = Distance + Weight
+INTERNEX = 
+if ALL == Weight <= 2 and Distance <= 100:
+	print('Free Shipping')
+
+if Total == 
+
+
+
+
