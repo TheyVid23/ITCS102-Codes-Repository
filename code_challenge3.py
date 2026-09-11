35e4fd84d05669fd193eebd779ec6a969e2ad961
TheyVid23 <deyvidanthonybalubal@gmail.com>
2026-09-11 11:13:50 +0800

    Refactor input handling for shipping details

--- code_challenge3.py
+++ code_challenge3.py
@@ -3,25 +3,25 @@ SenderName = input('What is your name? --> ')
 
 TypeOfItem = input('What is your order? --> ')
 
-is_Fragile = input('Is it fragile? (yes/no) --> ')
+is_Fragile = bool(input('Is it fragile? (yes/no) --> ') == 'yes')
 
 Weight = float(input('How heavy is it in kg? --> '))
 
 Distance = float(input('How far is it from the designated destination in km? --> '))
 
-is_Express = input('Is it in a hurry? (yes/no) --> ')
+is_Express = bool(input('Is it in a hurry? (yes/no) --> ') == 'yes')
 
-is_International = input('Is it from another country? (yes/no) --> ')
+is_International = bool(input('Is it from another country? (yes/no) --> ') == 'yes')
 
 base_cost = (Weight * 2.5) + (Distance * 0.15)
 
 if Distance <= 100 and Weight <= 2 and not is_Express and not is_International:
     Total = 0
 
-elif is_International == 'yes' and is_Express == 'yes':
+elif is_International and is_Express:
     Total = (base_cost * 1.4) + 50
 
-elif Weight > 20 and is_International == 'yes' or is_Express == 'yes':
+elif Weight > 20 and is_International or is_Express:
     Total = (base_cost * 1.2) + 25
 
 elif Distance > 1000 or Weight > 30:
